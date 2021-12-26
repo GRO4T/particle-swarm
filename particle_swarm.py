750936160b6de010855c3a6e400413a97409d7a1
Mateusz Dryjański <m.dryjanski@stud.elka.pw.edu.pl>
2021-12-26 10:16:35 +0100

    Add various conditions and omega_update methods. Add new class parameters

--- particle_swarm.py
+++ particle_swarm.py
@@ -6,12 +6,11 @@ from logger import configure_logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-configure_logging()
 
-
+logging.debug("Start")
 
 class ParticleSwarm:
-    def __init__(self, n_particles: int, objective_func, w: float = 0.8, c_local: float = 0.1, c_global: float = 0.1):
+    def __init__(self, n_particles: int, objective_func, w: float = 0.8, c_local: float = 0.1, c_global: float = 0.1, max_iteration: int = 100):
         """
         Parameters
         ----------
@@ -25,6 +24,8 @@ class ParticleSwarm:
             współczynnik przyciągania do lokalnego rozwiązania
         c_global: float
             współczynnik przyciągania do globalengo rozwiązania
+        max_iteration: int
+            maksymalna liczba iteracji algorytmu
         """
         self.X = np.random.rand(2, n_particles) * 5
         self.V = np.random.randn(2, n_particles) * 0.1
@@ -33,10 +34,12 @@ class ParticleSwarm:
         self.l_best_vals = self.obj_func(self.X[0], self.X[1]) 
         self.g_best_X = self.l_best_X[:, self.l_best_vals.argmin()]
         self.g_best_vals = self.l_best_vals.min()
+        self.last_g_best_vals = list()
         self.w = w
         self.c_l = c_local
         self.c_g = c_global
         self.iteration = 0
+        self.max_iteration = max_iteration
         logger.debug(f"positions={self.X}")
         logger.debug(f"velocities={self.V}")
         logger.debug(f"local_best_positions={self.l_best_X}")
@@ -63,8 +66,31 @@ class ParticleSwarm:
         self.l_best_vals = np.array([self.l_best_vals, new_obj_func_vals]).min(axis=0)
         self.g_best_X = self.l_best_X[:, self.l_best_vals.argmin()]
         self.g_best_vals = self.l_best_vals.min()
+        self.add_g_best_val()
 
         self.iteration += 1
         logger.info(f"Iteration: {self.iteration}")
         logger.info(f"global_best_position={self.g_best_X}")
         logger.info(f"global_best_value={self.g_best_vals}")
+        logger.info(f"size={len(self.last_g_best_vals)}")
+
+    def update_omega_randomly(self):
+        self.w = np.random.random()
+
+    def update_omega_iteration(self):
+        self.w = 1 - self.iteration / self.max_iteration
+
+    def add_g_best_val(self):
+        if len(self.last_g_best_vals) == 10:
+            del self.last_g_best_vals[0]
+        self.last_g_best_vals.append(self.g_best_vals)
+
+    def stop_condition_iteration(self):
+        return self.iteration <= self.max_iteration
+
+    def stop_condition_best_value(self):
+        if len(self.last_g_best_vals) < 10:
+            return True
+        return abs(sum(self.last_g_best_vals) / 10 - self.g_best_vals) >= 0.000000001
+
+
